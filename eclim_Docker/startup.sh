ecbaa8d794efa35a21eed7ee4bdb5f0bb67cbdec
Henri <henri@alfred-12721.home>
2016-11-16 23:16:44 +0000

    unset of JAVA_TOOL_OPTIONS variable in Dockerfile

--- eclim_Docker/startup.sh
+++ eclim_Docker/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+unset JAVA_TOOL_OPTIONS
 Xvfb :1 -screen 0 1024x768x24 &
 DISPLAY=:1 /opt/eclipse/eclipse -clean -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/juno -installIU org.eclipse.wst.web_ui.feature.feature.group
 DISPLAY=:1 /opt/eclipse/eclimd
